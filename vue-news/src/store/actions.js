f58567b7d50625aa927650244b75c21f3ef5bfee
minjun.ha <haminjun0@gmail.com>
2020-05-18 00:00:20 +0900

    store 모듈화

---
+++ vue-news/src/store/actions.js
@@ -0,0 +1,32 @@
+import { fetchNewsList, fetchJobsList, fetchAsksList } from '../api/index.js';
+
+export default {
+    FETCH_NEWS({ commit }) {
+        fetchNewsList()
+        .then(({ data }) => {
+            commit('SET_NEWS', data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    },
+    FETCH_JOBS({ commit }) {
+        fetchJobsList()
+        .then(({ data }) => { 
+            commit('SET_JOBS', data);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    },
+    FETCH_ASKS({ commit }) {
+        fetchAsksList()
+        // => 함수는 호출부의 this를, function 으로 묶은 건 다른 바인딩 this
+        .then(({data}) => { 
+            commit('SET_ASKS', data);
+        })
+        .catch(error => {
+            console.log(error);
+        }) 
+    }
+}
--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { fetchNewsList } from '../api/index.js';
-import { fetchJobsList } from '../api/index.js';
-import { fetchAsksList } from '../api/index.js';
+
+import mutations from './mutations.js';
+import actions from './actions.js'
 
 Vue.use(Vuex);
 
@@ -18,47 +18,8 @@ export const store = new Vuex.Store({
             return state.asks;
         }
     },
-    mutations: {
-        SET_NEWS(state, news) {
-            state.news = news;
-        },
-        SET_JOBS(state, jobs) {
-            state.jobs = jobs;
-        },
-        SET_ASKS(state, asks) {
-            state.asks = asks;
-        }
-    },
-    actions: {
-        FETCH_NEWS({ commit }) {
-            fetchNewsList()
-            .then(({ data }) => {
-                commit('SET_NEWS', data);
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        },
-        FETCH_JOBS({ commit }) {
-            fetchJobsList()
-            .then(({ data }) => { 
-                commit('SET_JOBS', data);
-            })
-            .catch(error => {
-                console.log(error);
-            })
-        },
-        FETCH_ASKS({ commit }) {
-            fetchAsksList()
-            // => 함수는 호출부의 this를, function 으로 묶은 건 다른 바인딩 this
-            .then(({data}) => { 
-                commit('SET_ASKS', data);
-            })
-            .catch(error => {
-                console.log(error);
-            }) 
-        }
-    },
+    mutations,
+    actions,
     // getters,
     // mutations,
     // actions,
