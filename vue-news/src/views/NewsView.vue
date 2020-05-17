24549c189eee4d311361ff311f6b2bbfdfc6b637
minjun.ha <haminjun0@gmail.com>
2020-05-17 18:47:11 +0900

    axios 호출 추가

--- vue-news/src/views/NewsView.vue
+++ vue-news/src/views/NewsView.vue
@@ -1,12 +1,29 @@
 <template>
     <div>
-        News
+        <div v-for="user in users">{{ user.title }}</div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    
+    data() {
+        return {
+            users: []
+        }
+    },
+    created() {
+        var vm = this;
+        axios.get('https://api.hnpwa.com/v0/news/1.json')
+        .then(response => { 
+            console.log(response);
+            vm.users = response.data;
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
 }
 </script>
 
