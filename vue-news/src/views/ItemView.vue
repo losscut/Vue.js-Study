0a0b4e102abacce15c71eadae85303c973ce6a85
minjun.ha <haminjun0@gmail.com>
2020-05-24 15:47:21 +0900

    css 스타일링

--- vue-news/src/views/ItemView.vue
+++ vue-news/src/views/ItemView.vue
@@ -1,8 +1,32 @@
 <template>
     <div>
-        <p>title : {{ itemInfo.title }}</p>
-        <p>user : {{ itemInfo.user }}</p>
-        <p>content : {{ itemInfo.content }}</p>
+        <section>
+            <!-- 질문 상세 정보 -->
+            <div class="user-container">
+                <div>
+                    <i class="fas fa-users"></i>
+                </div>
+                <div class="user-description">
+                    <router-link v-bind:to="`/user/${itemInfo.user}`">
+                        {{ itemInfo.user }}
+                    </router-link>
+                    <div class="time">
+                        {{ itemInfo.time_ago }}
+                    </div>
+                </div>
+            </div>
+
+            <h2>
+                {{ itemInfo.title }}
+            </h2>
+        </section>
+        
+        <section>
+            <!-- 질문 댓글 -->
+            <div v-html="itemInfo.content">
+
+            </div>
+        </section>
     </div>
 </template>
 
@@ -20,6 +44,19 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.user-container {
+    display: flex;
+    align-items: center;
+    padding: 0.5rem;
+}
+.fa-users {
+    font-size: 2.5rem;
+}
+.user-description {
+    padding-left: 8px;
+}
+.time {
+    font-size: 0.7rem;
+}
 </style>
