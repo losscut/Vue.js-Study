6cd7115a8980f4c3bab977503c213e64f8ffd721
minjun.ha <haminjun0@gmail.com>
2020-08-02 15:31:17 +0900

    listItem template v-if 조건 분기

--- vue-news/src/views/JobsView.vue
+++ vue-news/src/views/JobsView.vue
@@ -13,30 +13,3 @@ export default {
     },
 }
 </script>
-
-<style scoped> 
-.news-list {
-    margin : 0;
-    padding: 0;
-}
-.post {
-    list-style: none;
-    display: flex;
-    align-items: center;
-    border-bottom: 1px solid #eee;
-}
-.points {
-    width: 80px;
-    height: 60px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    color: #42b883;
-}
-.news-title {
-    margin : 0;
-}
-.link-text {
-    color: #828282;
-}
-</style>
